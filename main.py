fcb19e1cca5ba2952e3e16737b62751a24ddbc97
zhenshan <zhenshan@belmont.tech>
2022-03-29 09:35:23 -0500

    remove extra compuatation

--- main.py
+++ main.py
@@ -41,10 +41,8 @@ def run(image):
     use_gpu = False
 
     # load colorizers
-    colorizer_eccv16 = eccv16(pretrained=True).eval()
     colorizer_siggraph17 = siggraph17(pretrained=True).eval()
     if(use_gpu):
-        colorizer_eccv16.cuda()
         colorizer_siggraph17.cuda()
 
     # default size to process images is 256x256
@@ -56,8 +54,6 @@ def run(image):
 
     # colorizer outputs 256x256 ab map
     # resize and concatenate to original L channel
-    img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
-    out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
     out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
     # eccv16_image, siggraph17_image, comparison_image = save_image(out_img_eccv16, out_img_siggraph17, img, img_bw)
